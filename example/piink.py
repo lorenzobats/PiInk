31d0b1463393790826180a289a9fd7e7bf614562
lorenzobats <lorenzo.reloaded@web.de>
2024-08-14 18:51:51 +0100

    weather widget

--- example/piink.py
+++ example/piink.py
@@ -221,10 +221,11 @@ class Weather:
         (width, height) = size
         ctx.rectangle((0, 0, width, height), fill=255, outline=0, width=2)
         font36 = ImageFont.truetype('../fonts/FiraMono-Regular.ttf', 36)
-        font20 = ImageFont.truetype('../fonts/FiraMono-Regular.ttf', 20)
-        ctx.text((120, 30), f'{self.weather_data.temperature}°C', font=font36)
-        ctx.text((120, 70), f"{self.weather_data.desc}", font=font20)
-        ctx.text((120, 90), f"H: {self.weather_data.max}°C // T: {self.weather_data.min}°C", font=font20)
+        font24 = ImageFont.truetype('../fonts/FiraMono-Regular.ttf', 24)
+        ctx.text((130, 50), f'{self.weather_data.temperature}°C', font=font36)
+        ctx.text((130, 90), f'{self.weather_data.desc}', font=font24)
+        ctx.text((130, 120), f'H: {self.weather_data.max}°C')
+        ctx.text((130, 150), f'T: {self.weather_data.min}°C', font=font24)
 
 
 @dataclass(slots=True)
@@ -249,9 +250,8 @@ async def ui_handler(event_queue: asyncio.Queue):
 
     ctx = EventCtx(event_queue=event_queue, scheduled_tasks=dict())
     widgets: dict[int, (Any, (int, int, int, int))] = dict([
-        (0, (Clock(), (0, 0, 800, 30))),
-        (1, (Greeter(), (0, 160, 800, 320))),
-        (2, (Weather(), (0, 30, 400, 150)))
+        (0, (Clock(), (0, 0, 400, 240))),
+        (1, (Weather(), (0, 240, 400, 240)))
     ])
 
     for (widget_id, (widget, (x, y, width, height))) in widgets.items():
